bcee6ae4e5b2ee06c31cac8629d5d3f49217316d
Sundram Kumar <sundramroy1208@gmail.com>
2025-02-20 10:45:09 +0530

    Time: 3 ms (42.66%), Space: 10.5 MB (59.85%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,12 +1,25 @@
 class Solution {
+
+    string leadAndTrailSpaces(string str){
+        size_t start = str.find_first_not_of(' ');
+        size_t end = str.find_last_not_of(' ');
+    
+        if (start == std::string::npos)
+            return "";
+    
+        return str.substr(start, end - start + 1);
+    }
+
 public:
     string reverseWords(string s) {
-        string result = regex_replace(s, regex("^ +| +$|( ) +"), "$1");
+        string result = leadAndTrailSpaces(s);
+        // string result = regex_replace(s, regex("^ +| +$|( ) +"), "$1");
         int n = result.length();
         stack<char> st;
         string ans;
-        for(int i=n-1;i>=-1;i--){
-            if(i==-1 || result[i]==' '){
+        for(int i=n-1;i>=0;i--){
+            if(result[i]==' ' && result[i-1]==' ') continue;
+            else if(i==-1 || result[i]==' '){
                 while(!st.empty()){
                     ans.push_back(st.top());
                     st.pop();
@@ -20,6 +33,10 @@ public:
                 st.push(result[i]);
             }
         }
+        while(!st.empty()){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
         return ans;
     }
 };
