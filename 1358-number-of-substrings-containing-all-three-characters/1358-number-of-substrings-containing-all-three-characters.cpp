d8fb33a6b59ed36677ca116e661e7177d40cfb55
Sundram Kumar <sundramroy1208@gmail.com>
2025-03-15 23:53:49 +0530

    Time: 10 ms (55.47%), Space: 10.8 MB (95.33%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int cnt = 0;
-        int left = 0;
-        int hash[3] = {0};
+        // int left = 0;
+        int hash[3] = {-1,-1,-1};
         for(int i=0;i<s.length();i++){
-            hash[s[i]-'a']++;
-            while(hash[0]>0 && hash[1]>0 && hash[2]>0 && left<i){
-                hash[s[left]-'a']--;
-                left++;
-                cnt+=(s.length()-i);
+            hash[s[i]-'a'] = i;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                cnt+= min(hash[0],min(hash[1],hash[2]))+1;
             }
         }
         return cnt;
